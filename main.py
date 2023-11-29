cc6eb055eb6068f5c0dcdc1082e3fe8b53b73e87
Brandon Moy <brandonkaghenmoy@gmail.com>
2023-11-29 13:29:37 -0800

    Defined functions for listing patients and starting diagnoses and replaced comments with function calls in flow

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
 welcome_prompt = "Hello! What would you like to do today?\n - List all patients, press 1\n - To run a new diagnosis, press 2\n - To quit, press q\n"
 
-def main():
-  selection = input(welcome_prompt)
-  print(selection)
+def list_patients ():
+    print("List of patients and diagnoses")
+    
+def start_diagnosis ():
+    print("Starting a new diagnosis")    
+
+
+def main ():
+    selection = input(welcome_prompt)
+    if selection == "1":
+        list_patients()
+    elif selection == "2":
+        start_diagnosis()
+    elif selection == "q":
+        return
   
 main()
