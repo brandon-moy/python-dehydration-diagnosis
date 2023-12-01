8d36ee72d2b82dde330b55c7adb3dc8d04167222
Brandon Moy <brandonkaghenmoy@gmail.com>
2023-12-01 10:33:08 -0800

    Added prompts for name and appearance as inputs for start_diagnosis function

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 welcome_prompt = "Hello! What would you like to do today?\n - List all patients, press 1\n - To run a new diagnosis, press 2\n - To quit, press q\n"
+name_prompt = "What is the patient's name?\n"
+appearance_prompt = "How is the patient's general appearance?\n - 1: Normal appearance \n -2: Irritable or lethargic\n"
 
 def list_patients ():
     print("List of patients and diagnoses")
     
 def start_diagnosis ():
-    print("Starting a new diagnosis")    
-
+    name = input(name_prompt)
+    appearance = input(appearance_prompt)
 
 def main ():
     while(True):
