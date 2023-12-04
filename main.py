2a9a2b4310028324fff73f17f339207cc01b42bc
Brandon Moy <brandonkaghenmoy@gmail.com>
2023-12-04 11:17:37 -0800

    Added error handling is no inputs were provided by user and an error message to use if needed

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ appearance_prompt = "How is the patient's general appearance?\n - 1: Normal appe
 eye_prompt = "How are the patient's eyes?\n - 1: Eyes normal or slightly sunken\n - 2: Eyes very sunken\n"
 skin_prompt = "How is the patient's skin when you pinch it?\n - 1: Normal skin pinch\n - 2: Slow skin pinch\n"
 
+error_message = "Could not save patient and diagnosis due to invalid input"
+
 severe_dehydration = "Severe dehydration"
 some_dehydration = "Some dehydration"
 no_dehydration = "No dehydration"
@@ -20,6 +22,9 @@ def list_patients ():
         print(patient)
     
 def save_new_diagnosis(name, diagnosis):
+    if name == "" or diagnosis == "":
+        print(error_message)
+        return
     final_diagnosis = name + " - " + diagnosis
     patients_and_diagnoses.append(final_diagnosis)
     print("Final diagnosis:", final_diagnosis, "\n")
@@ -30,12 +35,16 @@ def assess_eyes():
         return no_dehydration
     elif eyes == "2":
         return severe_dehydration
+    else:
+        return ""
     
 def assess_skin(skin):
     if skin == "1":
         return some_dehydration
     elif skin == "2":
         return severe_dehydration
+    else:
+        return ""
     
 def assess_appearance():
     appearance = input(appearance_prompt)
@@ -45,6 +54,8 @@ def assess_appearance():
     elif appearance == "2":
         skin = input(skin_prompt)
         return assess_skin(skin)
+    else:
+        return ""
     
 def start_diagnosis ():
     name = input(name_prompt)
